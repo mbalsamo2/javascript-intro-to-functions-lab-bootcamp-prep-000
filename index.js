92a14cc4d585d93520fdad8a226aac84c6aab811
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-01 21:20:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+  if () {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
   string.toLowerCase 
   return "I can\'t hear you!"
 }
